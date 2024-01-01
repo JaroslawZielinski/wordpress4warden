3007650b3fcf2ab08cfed2bbfe7e4531372c0038
Jarosław Zieliński <jaroslaw.zielinski@polcode.net>
2024-01-02 00:57:01 +0100

    Fixes 04

--- includes/fix-dbpermissions.sh
+++ includes/fix-dbpermissions.sh
@@ -2,4 +2,6 @@
 source includes/core.sh
 warden env exec -u root -T php-fpm bash -c "sudo chown www-data:www-data -R /var/www/html/data"
 warden env exec -u root -T php-fpm bash -c "sudo chmod 777 -R /var/www/html/data"
+warden env exec -u root -T php-fpm bash -c "sudo chown www-data:www-data -R /var/www/html/"
+warden env exec -u root -T php-fpm bash -c "sudo chmod 777 -R /var/www/html/"
 :: "Fixed Db Permissions."
