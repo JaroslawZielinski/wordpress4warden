bf5393573153f3bd7a18bbfab8defc86012bda16
Jarosław Zieliński <jaroslaw.zielinski@polcode.net>
2024-01-02 06:31:54 +0100

    Fixes 07

--- includes/dbip.sh
+++ includes/dbip.sh
@@ -2,4 +2,5 @@
 source includes/core.sh
 mysqlIP=$(docker inspect --format "{{with index .NetworkSettings.Networks \"${WARDEN_ENV_NAME}_default\"}}{{.IPAddress}}{{end}}" ${WARDEN_ENV_NAME}-db-1)
 echo "DB_HOST=${mysqlIP}" > dbip.conf
+yes | cp .warden/wp-config.php wp-config.php
 :: $mysqlIP
